8c02a37e79d5897ea1fab5cf20fb2a60996e89c1
ilutreksl <carlos@ilutrek.es>
2025-11-04 22:19:21 +0100

    0.0.66 - Cambiado icono de reportar por el copia de seguridad

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ DB_NAME = "rma_app.db"
 # Mensaje de advertencia sobre la limitación de SQLite en red compartida
 ADVERTENCIA_MULTIUSUARIO = "⚠️ ADVERTENCIA: Esta app usa SQLite, NO es segura para múltiples usuarios escribiendo a la vez en red compartida. ¡Riesgo de corrupción de datos si escriben a la vez!"
 
-APP_VERSION = "v0.0.65"
+APP_VERSION = "v0.0.66"
 DB_FILENAME = "rma_app.db"
 
 # Session global para Turso (reutiliza conexiones HTTP)
@@ -1086,7 +1086,7 @@ class VentanaPrincipal(ctk.CTkToplevel):
         if rol_norm in ("admin", "administrador", "dpto. tecnico", "dpto tecnico", "dpto técnico"):
             self.btn_buscar = ctk.CTkButton(self.sidebar_frame,
                                            text="",
-                                           image=self.icon_reports,
+                                           image=self.icon_info,
                                            width=44,
                                            height=44,
                                            fg_color=sidebar_bg,
@@ -1098,7 +1098,7 @@ class VentanaPrincipal(ctk.CTkToplevel):
 
         self.btn_reportar = ctk.CTkButton(self.sidebar_frame,
                                           text="",
-                                          image=self.icon_info,
+                                          image=self.icon_reports,
                                           width=44,
                                           height=44,
                                           fg_color=sidebar_bg,
